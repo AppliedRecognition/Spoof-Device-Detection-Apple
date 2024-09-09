482e011011b45ad2bfbc2fedb1aaf07b613d2b9e
Jakub Dolejs <jakubdolejs@gmail.com>
2024-09-09 14:42:53 +0100

    Switched to ARC_PSD-001_1.4.151_bst_yl82087_NMS_ult087_cml72 model

--- Sources/SpoofDeviceDetectionModel/SpoofDeviceDetectionModel.swift
+++ Sources/SpoofDeviceDetectionModel/SpoofDeviceDetectionModel.swift
@@ -11,7 +11,7 @@ import Foundation
 public extension SpoofDeviceDetector {
     
     convenience init() throws {
-        guard let url = Bundle.module.url(forResource: "ARC_PSD-001_1.1.122_bst_yl80201_NMS_ult201_cml70", withExtension: "mlmodelc") else {
+        guard let url = Bundle.module.url(forResource: "ARC_PSD-001_1.4.151_bst_yl82087_NMS_ult087_cml72", withExtension: "mlmodelc") else {
             throw NSError() // TODO
         }
         try self.init(compiledModelURL: url, identifier: "ARC_PSD-001")
